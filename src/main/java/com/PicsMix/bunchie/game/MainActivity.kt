14c57dab94f7a6fde4f8e11516ccf1c9ea7d2501
LamLieu <lieulam@hotmail.com>
2021-05-03 13:31:04 -0700

    PicsMix Kotlin Base

---
+++ src/main/java/com/PicsMix/bunchie/game/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.PicsMix.bunchie.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ src/main/java/com/PicsMix/bunchie/game/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.PicsMix.bunchie.game
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val logo = findViewById<ImageView>(R.id.logo)
+        logo.alpha = 0f
+        logo.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
